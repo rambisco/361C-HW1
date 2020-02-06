f5269ab6f82ce331c98a29466ff72c6e6a4a754f
rambisco <david.wolf.tx@gmail.com>
2020-02-06 00:02:33 -0600

    bakery

--- src/q6/Bakery/BakeryLock.java
+++ src/q6/Bakery/BakeryLock.java
@@ -1,17 +1,73 @@
 package q6.Bakery;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class BakeryLock implements Lock {
+
+    public static int getN() {
+        return n;
+    }
+
+    public static void setN(int n) {
+        BakeryLock.n = n;
+    }
+
+    public static AtomicBoolean[] getChoosing() {
+        return choosing;
+    }
+
+    public static void setChoosing(AtomicBoolean[] choosing) {
+        BakeryLock.choosing = choosing;
+    }
+
+    public static AtomicInteger[] getNumber() {
+        return number;
+    }
+
+    public static void setNumber(AtomicInteger[] number) {
+        BakeryLock.number = number;
+    }
+
+    static int n;
+    static AtomicBoolean[] choosing;
+    static AtomicInteger[] number;
+
     public BakeryLock(int numThreads){
         // your implementation goes here.
+        setN(numThreads);
+        setChoosing(new AtomicBoolean[n]);
+        setNumber(new AtomicInteger[n]);
+
+        for(int i = 0; i < n; i++) {
+            choosing[i] = new AtomicBoolean(false);
+            number[i] = new AtomicInteger(0);
+        }
     }
 
     @Override
     public void lock(int pid) {
+        choosing[pid].set(true);
+        for(int i = 0; i < n; i++) {
+            if (number[i].get() > number[pid].get()) {
+                number[pid].set(number[i].get());
+            }
+        }
+        number[pid].set(number[pid].get()+1);
+        choosing[pid].set(false);
+        //step 2
+        for(int j = 0; j < n; j++){
+            while(choosing[j].get()){};
+            while((number[j].get() != 0) &&
+                    (number[j].get() < number[pid].get()) ||
+                    (number[j].get() == number[pid].get()) && (j < pid)){
 
+                    }
+        }
     }
 
     @Override
     public void unlock(int pid) {
-
+        number[pid].set(0);
     }
 }
--- src/q6/Bakery/PIncrement.java
+++ src/q6/Bakery/PIncrement.java
@@ -1,13 +1,82 @@
 package q6.Bakery;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class PIncrement implements Runnable{
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        PIncrement.count = count;
+    }
+
+    public static int getN() {
+        return n;
+    }
+
+    public static void setN(int n) {
+        PIncrement.n = n;
+    }
+
+    public static int getM() {
+        return m;
+    }
+
+    public static void setM(int m) {
+        PIncrement.m = m;
+    }
+
+    static int count;
+    static int n;
+    static int m;
+    static HashMap<Long, Integer> threads;
+    static ArrayList<Thread> ts;
+    static BakeryLock lock;
+
+
     public static int parallelIncrement(int c, int numThreads){
+
+        setCount(c);
+        setN(numThreads);
+        setM(1200000);
+        lock = new BakeryLock(numThreads);
+        threads = new HashMap<>();
+        ts = new ArrayList<>();
+
+        for(int i = 0; i < numThreads; i++){
+            Thread t = new Thread(new PIncrement());
+            threads.put(t.getId(), i);
+            ts.add(t);
+            t.start();
+        }
+        for(Thread t : ts){
+            try {
+                t.join();
+            }
+            catch(Exception e){
+                System.out.println("bad stuff");
+            }
+        }
+
+
         // your implementation goes here
-        return 0;
+
+        return count;
     }
 
     @Override
     public void run() {
-
+        int pid = threads.get(Thread.currentThread().getId());
+        System.out.println("index: " + pid);
+        for(int i = 0; i < (m/n); i++){
+            lock.lock(pid);
+            setCount(getCount()+1);
+            lock.unlock(pid);
+        }
     }
 }
